bf117ccf73c9f7b9a5f3ba3b6178d1527854a344
aomine <aomine1745@gmail.com>
2020-12-31 19:08:38 -0500

    v 1.0

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document.querySelector("body").addEventListener("mousemove", eyeball);
+
+function eyeball(event) {
+  const eye = document.querySelectorAll(".eye");
+  eye.forEach(function(eye) {
+    const x = eye.getBoundingClientRect().left + (eye.clientWidth / 2);
+    const y = eye.getBoundingClientRect().top + (eye.clientHeight / 2);
+
+    const radian = Math.atan2(event.pageX - x, event.pageY - y);
+
+    const rot = (radian * (180 / Math.PI) * -1) + 270;
+
+    eye.style.transform = `rotate(${rot}deg)`;
+  });
+}
